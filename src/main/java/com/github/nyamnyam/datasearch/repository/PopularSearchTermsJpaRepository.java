555e0e33cb48ebe7fd8bc94068720b4919248bae
ari <suhyeon@stclab.com>
2023-03-21 13:54:53 +0900

    README 수정

--- src/main/java/com/github/nyamnyam/datasearch/repository/PopularSearchTermsJpaRepository.java
+++ src/main/java/com/github/nyamnyam/datasearch/repository/PopularSearchTermsJpaRepository.java
@@ -3,14 +3,22 @@ package com.github.nyamnyam.datasearch.repository;
 import com.github.nyamnyam.datasearch.domain.PopularSearchTerms;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.LockModeType;
 import java.util.List;
 
 public interface PopularSearchTermsJpaRepository extends JpaRepository<PopularSearchTerms, String>  {
 
-    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Lock(value = LockModeType.PESSIMISTIC_WRITE)
     PopularSearchTerms findAllByQuery(String query);
+
+//    @Lock(LockModeType.PESSIMISTIC_WRITE)
+//    //@Query("INSERT INTO account (id, amount) VALUES (:id, :someValue) ON DUPLICATE KEY UPDATE amount = account.amount + :someValue")
+//    @Query("insert INTO popular_search_terms (query, query_count) VALUES (:query, 1) ON DUPLICATE KEY UPDATE query_count = query + 1")
+//    void updateQueryCount(@Param("query") String query);
 
     List<PopularSearchTerms> findTop10ByOrderByQueryCountDescQueryAsc();
 
